fbc7e64f3f5ab26d407cbd60cb13316a2ded75e7
Sebastian Khan <KhanS22@Cardiff.ac.uk>
2019-11-26 09:16:16 +0000

    doc string

--- examples/run-and-print-profile.sh
+++ examples/run-and-print-profile.sh
@@ -1,2 +1,5 @@
+
+# run this with
+# bash run-and-print-profile.sh
 python -m cProfile -o profiling_results wf-gen-profile.py
 python -c "import pstats; stats = pstats.Stats('profiling_results'); stats.sort_stats('tottime'); stats.print_stats(20)"
